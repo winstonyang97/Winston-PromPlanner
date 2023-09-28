233cb476911fdb6a9011dad709505f8388337038
winstonyang97 <theicemen19@gmail.com>
2023-09-28 10:25:00 -0400

    Initial Commit

---
+++ Winston-PromPlanner/Winston_PromPlannerApp.swift
@@ -0,0 +1,20 @@
+//
+//  Winston_PromPlannerApp.swift
+//  Winston-PromPlanner
+//
+//  Created by Winston Yang on 2023-09-28.
+//
+
+import SwiftUI
+
+@main
+struct Winston_PromPlannerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
